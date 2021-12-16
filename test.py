f197f82f10e9d71a4e2a12a55ae2a9d17b9a0a3b
oiloilman <95857734+oiloilman@users.noreply.github.com>
2021-12-16 21:06:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import requests
 from io import BytesIO
-st.write(input(url))
+url=st.write(input('url'))
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
