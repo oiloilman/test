05e4d2161b8e3ceb5a6c361639db4ca0ecabfb75
oiloilman <95857734+oiloilman@users.noreply.github.com>
2021-12-16 21:29:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from PIL import Image
 
-st.write("## Image example:- ")
+st.text_input("## Image example:- ")
 
-img = Image.open(".jpeg")
+img = Image.open("dog.jpeg")
 st.image(img)
