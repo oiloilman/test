6fd997dc35d5e70da25a3cff047fa0ab0ec6c61e
oiloilman <95857734+oiloilman@users.noreply.github.com>
2021-12-16 20:56:50 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-import requests
-import io
 from PIL import Image
+import requests
+from io import BytesIO
 
-response = requests.get("https://i.imgur.com/ExdKOOz.png")
-image_bytes = io.BytesIO(response.content)
-
-img = Image.open(image_bytes)
-print(f'Size: {img.size}')
-img.show()
+response = requests.get(url)
+img = Image.open(BytesIO(response.content))
