d2c7ef6fad2b2d34fb236065a8225cfb5a818248
oiloilman <95857734+oiloilman@users.noreply.github.com>
2021-12-16 20:46:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-import shutil
-import requests
-
-my_url = 'https://www.washingtonian.com/wp-content/uploads/2017/06/6-30-17-goat-yoga-congressional-cemetery-1-994x559.jpg'
-response = requests.get(my_url, stream=True)
-with open('my_image.png', 'wb') as file:
-    shutil.copyfileobj(response.raw, file)
-del response
-from PIL import Image
-
-img = Image.open('my_image.png')
-img.show()
+from imageio import imread
+image = imread('https://cdn.sstatic.net/Sites/stackoverflow/img/logo.png')
