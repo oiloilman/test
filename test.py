2ef6d1990c35c2d31fe181c73ffa3f0a885ff3d3
oiloilman <95857734+oiloilman@users.noreply.github.com>
2021-12-16 21:20:13 +0800

    Create test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from PIL import Image
 import requests
 from io import BytesIO
 import streamlit as st
-url=st.text_input('url')
+url=st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None)('url')
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
+show(img)
