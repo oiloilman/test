fecaf0ab73c47688b09c71752f9a0f7f08a46ef6
oiloilman <95857734+oiloilman@users.noreply.github.com>
2021-12-16 20:41:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-[theme]
-base="dark"
-primaryColor="#f98a23"
-font="monospace"
+from PIL import Image
+import cv2
+import numpy as np
+import requests
+image=Image.open(requests.get("https://previews.123rf.com/images/darrenwhi/darrenwhi1310/darrenwhi131000024/24022179-photo-of-many-cars-with-one-a-different-color.jpg", stream=True).raw)
+#image =resize((420,250))
+
+image_array=np.array(image)
+image 
