7c1a51d6e8f311ad993f3faef2844fcd9ae6963b
oiloilman <95857734+oiloilman@users.noreply.github.com>
2021-12-16 21:25:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from PIL import Image
-import requests
-from io import BytesIO
 import streamlit as st
-url=st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None)('url')
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
-show(img)
+
+from PIL import Image
+
+st.write("## Image example:- ")
+
+img = Image.open("dogs.jpeg")
+st.image(img)
