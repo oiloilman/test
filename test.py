40f55a40ffc8df6840a5e9ab7a76f67f1e9b9cfb
oiloilman <95857734+oiloilman@users.noreply.github.com>
2021-12-16 21:39:07 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-import streamlit as st
-
-from PIL import Image
-
-st.text_input("## Image example:- ")
-
-img = Image.open("dog.jpeg")
-st.image(img)
+st.video("https://www.youtube.com/watch?v=DrKLYvLPidw")
