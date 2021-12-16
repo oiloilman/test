4a2864c8d558a433a4a921d21965a35a227c5310
oiloilman <95857734+oiloilman@users.noreply.github.com>
2021-12-16 21:09:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import requests
 from io import BytesIO
+import streamlit as st
 url=st.write(input('url'))
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
